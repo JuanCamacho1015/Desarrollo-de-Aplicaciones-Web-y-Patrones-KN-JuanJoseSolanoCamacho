39370453fba58f7246e1fe80cae9c6ab5f8dd46b
JJCB10 <juanc.bj2002@gmail.com>
2026-02-10 20:37:55 -0600

    Commit semana 5

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -13,34 +13,60 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.io.Serializable;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-@Data
-@Getter
-@Setter
 @Entity
 @Table (name ="categoria")
 public class Categoria implements Serializable {
-// Se recomienda añadir un serialVersionUID
-private static final long serialVersionUID = 1L;
-@Id
-@GeneratedValue (strategy = GenerationType.IDENTITY)
-@Column(name = "id_categoria")
-public Integer idCategoria;
+    // Se recomienda añadir un serialVersionUID
+    private static final long serialVersionUID = 1L;
 
-@Column (unique = true, nullable = false, length = 50)
-@NotNull
-@Size (max = 50)
-public String descripcion;
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
 
-@Column (length = 1024)
-@Size (max = 1024)
-public String rutaImagen;
+    @Column (unique = true, nullable = false, length = 50)
+    @NotNull
+    @Size (max = 50)
+    private String descripcion;
 
-@Column (name = "activo")
-public Boolean activo;
+    @Column (length = 1024)
+    @Size (max = 1024)
+    private String rutaImagen;
 
+    @Column (name = "activo")
+    private Boolean activo;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
 }
 
